f8f93f3d1efdb6f537aa84f8545f4ecaa05ff43e
unknown <sumbadrenan619@gmail.com>
2026-01-30 17:32:59 +0800

    semi working

--- script.js
+++ script.js
@@ -36,37 +36,64 @@ async function PDFToText(){
 
         }
         const slicedOriginal = parts.slice(start + 1, end);
+        const finalSlice = slicedOriginal.filter(item => item != " ");
         const units = new Set(['1.00','2.00','3.00','4.00','5.00','6.00','7.00','8.00','9.00','10.00'])
         const days = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+        const daysOfWeek = {
+            0 : ["SUN"],
+            1 : ['MONDAY', []],
+            2 : ['TUESDAY',[]],
+            3 : ['WEDNESDAY',[]],
+            4 : ['THURSDAY', []],
+            5 : ['FRIDAY',[]],
+            6 : ['SATURDAY',[]]
+        }
         let valid = []
-        let courseCodes = {}
-        let daysOfWeek = {}
+        let daysOfWeekPosition;
 
         function sliceCut(sliced){
-            let i = 0
-            while(!units.has(sliced[i].trim()) && i <= sliced.length){
+            if(sliced.length >= 0){
+                let dayCount = 0;
+                let newClass = [];
+                 let i = 0
+                while(!units.has(sliced[i].trim()) && i <= sliced.length){
                     if(i === 0){
                         let currentCode = sliced[i].trim()
-                        let currentSubject = sliced[1].slice(currentCode.length + 1);
-                        courseCodes[currentCode] = currentSubject;
+                        let currentSubject = (sliced[1].slice(currentCode.length + 1));
+                        newClass.push(currentCode);
+                        newClass.push(currentSubject);
                     }
-                    else if(days.includes(sliced[i])){
-                        daysOfWeek[days.indexOf(sliced[i].trim())] = 
+                    else if(days.includes(sliced[i].trim())){
+                        if(dayCount === 0){
+                            dayCount++
+                            newClass.push(sliced[i + 1].trim())
+                            newClass.push(sliced[i + 2].trim())
+                            daysOfWeek[days.indexOf(sliced[i].trim())][1].push(newClass);
+                            while(newClass.length > 2){
+                                newClass.pop()
+                            }
+                        }
+                        else{
+                            newClass.push(sliced[i + 1].trim())
+                            newClass.push(sliced[i + 2].trim())
+                            daysOfWeek[days.indexOf(sliced[i].trim())][1].push(newClass);
+                        }
+                        
 
                     }
-
-                    valid.push(sliced[i])
-
                     i++
                 
             }
             return
 
+            }
+        
+
 
         }
-       sliceCut(slicedOriginal)
-       console.log(valid)
-       console.log(courseCodes)
+       sliceCut(finalSlice)
+       console.log(daysOfWeek)
+
         
         
     }
